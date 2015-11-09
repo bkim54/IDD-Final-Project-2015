dc5eb82bf4e9cfb3781cc25ccce658277ee0ed68
saurabhmitra <saurabh.mitra@berkeley.edu>
2015-11-09 15:09:40 -0800

    testing getting a dict

--- static/scripts/mouse.js
+++ static/scripts/mouse.js
@@ -1,5 +1,43 @@
 var Mouse = (function() {
 
+	var apiUrl = 'http://localhost:8080'
+ 	/**
+    * HTTP GET request 
+    * @param  {string}   url       URL path, e.g. "/api/smiles"
+    * @param  {function} onSuccess   callback method to execute upon request success (200 status)
+    * @param  {function} onFailure   callback method to execute upon request failure (non-200 status)
+    * @return {None}
+    */
+	var makeGetRequest = function(url, onSuccess, onFailure) {
+		$.ajax({
+		   type: 'GET',
+		   url: apiUrl + url,
+		   dataType: "json",
+		   success: onSuccess,
+		   error: onFailure
+		});
+	};
+
+    /**
+     * HTTP POST request
+     * @param  {string}   url       URL path, e.g. "/api/smiles"
+     * @param  {Object}   data      JSON data to send in request body
+     * @param  {function} onSuccess   callback method to execute upon request success (200 status)
+     * @param  {function} onFailure   callback method to execute upon request failure (non-200 status)
+     * @return {None}
+     */
+    var makePostRequest = function(url, data, onSuccess, onFailure) {
+        $.ajax({
+            type: 'POST',
+            url: apiUrl + url,
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            dataType: "json",
+            success: onSuccess,
+            error: onFailure
+        });
+    };
+
 	var motionBarGraph = function() {
 		var cxt = document.getElementById("motion-bar-graph").getContext("2d");
 		
@@ -31,6 +69,14 @@ var Mouse = (function() {
 	}
 
 	var motionTrackerGraph= function() {
+        var onSuccess = function(data) {
+        	console.log(data);
+        };
+        var onFailure = function() { 
+            console.error('error'); 
+        };
+		makeGetRequest(apiUrl + '/motion_pie', onSuccess, onFailure);
+		
 		var cxt = document.getElementById("motion-tracker-graph").getContext("2d");
 		
 		Chart.defaults.global.responsive = true;
