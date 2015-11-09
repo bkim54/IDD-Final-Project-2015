ab97008acd6e3d628795f7a61edb98fc17538768
saurabhmitra <saurabh.mitra@berkeley.edu>
2015-11-09 14:19:50 -0800

    stuff

--- scripts/mouse.js
+++ scripts/mouse.js
@@ -1,15 +1,61 @@
 var Mouse = (function() {
 
-	var start = function() {
+	var motionBarGraph = function() {
+		var cxt = document.getElementById("motion-bar-graph").getContext("2d");
+		
+		Chart.defaults.global.responsive = true;
+		Chart.defaults.global.tooltipFontSize = 20;
+		Chart.defaults.global.scaleFontSize = 20;
+		var data = {
+		    labels: ["8-9AM", "9-10AM", "10-11AM", "11-12PM", "12-1PM", "1-2PM", "2-3PM"],
+		    datasets: [
+		        {
+		            label: "My First dataset",
+		            fillColor: "rgba(220,220,220,0.5)",
+		            strokeColor: "rgba(220,220,220,0.8)",
+		            highlightFill: "rgba(220,220,220,0.75)",
+		            highlightStroke: "rgba(220,220,220,1)",
+		            data: [65, 59, 80, 81, 56, 55, 40]
+		        },
+		        {
+		            label: "My Second dataset",
+		            fillColor: "rgba(151,187,205,0.5)",
+		            strokeColor: "rgba(151,187,205,0.8)",
+		            highlightFill: "rgba(151,187,205,0.75)",
+		            highlightStroke: "rgba(151,187,205,1)",
+		            data: [28, 48, 40, 19, 86, 27, 90]
+		        }
+		    ]
+		};
+		var graph = new Chart(cxt).Bar(data, {});
+	}
+
+	var motionTrackerGraph= function() {
 		var cxt = document.getElementById("motion-tracker-graph").getContext("2d");
-		                        
-		var graph = new BarGraph(cxt);
-		graph.margin = 2;
-		graph.width = 570;
-		graph.height = 500;
-		graph.xAxisLabelArr = ["8-9","AM", "9-10","AM", "10-11","AM", "11-12","PM", "12-1","PM", "1-2","PM", "2-3","PM", "3-4","PM", "4-5","PM"];
-		graph.colors = ["blue", "black"]
-		graph.update([3, 5, 3, 4, 4, 13, 2, 4, 7, 3, 5, 3, 4, 4, 10, 2, 4, 7]);
+		
+		Chart.defaults.global.responsive = true;
+		Chart.defaults.global.tooltipFontSize = 20;
+		Chart.defaults.global.scaleFontSize = 20;
+		var data = [
+		    {
+		        value: 300,
+		        color:"#aaaaaa",
+		        highlight: "#FF5A5E",
+		        label: "Wrist"
+		    },
+		    {
+		        value: 50,
+		        color: "#76BFFD",
+		        highlight: "#5AD3D1",
+		        label: "Elbow"
+		    }
+		]
+		var graph = new Chart(cxt).Pie(data, {});
+	}
+
+	var start = function() {
+		motionBarGraph();
+		motionTrackerGraph();
 	};
 
 
