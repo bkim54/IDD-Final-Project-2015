c48530d548332cde71978276a57623e48833c291
Bill Kim <bkim54@Jhu.edu>
2015-11-09 14:58:48 -0800

    moved css stuff into static folder

--- readserial.py
+++ readserial.py
@@ -12,18 +12,7 @@ def checkInput(read, s):
 ACCEL_THRESH = 1.2 #1.2 for robustness, could just do 1
 GYRO_THRESH = 0.5
 connected=False
-try:
-    ser = serial.Serial('/COM7',9600)
-    connected = True
-    db = dataset.connect('sqlite:///nbedmbed.db')       
-    motion_table = db['motion']
-    
-    motion_table.delete()
-    force_table = db['force']
-    force_table.delete()
-except serial.SerialException:
-    print "No device connected"
-    connected=False
+
     
 count = 0;
 gyro_buffer=[0,0,0,0,0,0,0,0,0,0] 
@@ -32,67 +21,84 @@ oldKey = ""
 elbowCount = 0
 wristCount = 0
 
-while (connected):
-    try:
-        accel = ser.readline().split()
-    except serial.SerialException:
-        print "disconnected"
-        break
-    key = str(datetime.date.today()) + " " + str(datetime.datetime.now().hour)
-    if (checkInput(accel, "Accel:")):
-        accel = [float(accel[1]), float(accel[2])] #just take into accout x,y
-        accel = numpy.sqrt(numpy.sum(numpy.power(accel,2)))
+
         
+
+if __name__ == "__main__":    
+    try:
+        ser = serial.Serial('/COM7',9600)
+        connected = True
+        db = dataset.connect('sqlite:///nbedmbed.db')       
+        motion_table = db['motion']
+        
+        motion_table.delete()
+        force_table = db['force']
+        force_table.delete()
+    except serial.SerialException:
+        print "No device connected"
+        connected=False
+        
+    while (connected):
         try:
-            gyro = ser.readline().split()
+            accel = ser.readline().split()
         except serial.SerialException:
             print "disconnected"
             break
-        
-        gyro = [float(gyro[1]), float(gyro[2]), float(gyro[3])]
-        gyro = numpy.sqrt(numpy.sum(numpy.power(gyro,2)))
-        
-        gyro_buffer[count%10] = gyro
-        accel_buffer[count%10] = accel        
-        count = count+1
-        if (count ==10):
-            count = 0
-        gyro = numpy.sum(gyro_buffer)/10.0
-        accel = numpy.sum(accel_buffer)/10.0
-        #print "accel: " +str(accel)
-        #print "gyro: " + str(gyro)
-        if (accel > ACCEL_THRESH):
-            if (gyro > GYRO_THRESH):
-                if (key == oldKey):
-                    wristCount = wristCount+1
-                    motion_table.update(dict(date=key,elbow=elbowCount,wrist=wristCount),['date'])
+        key = str(datetime.date.today()) + " " + str(datetime.datetime.now().hour)
+        if (checkInput(accel, "Accel:")):
+            accel = [float(accel[1]), float(accel[2])] #just take into accout x,y
+            accel = numpy.sqrt(numpy.sum(numpy.power(accel,2)))
+            
+            try:
+                gyro = ser.readline().split()
+            except serial.SerialException:
+                print "disconnected"
+                break
+            
+            gyro = [float(gyro[1]), float(gyro[2]), float(gyro[3])]
+            gyro = numpy.sqrt(numpy.sum(numpy.power(gyro,2)))
+            
+            gyro_buffer[count%10] = gyro
+            accel_buffer[count%10] = accel        
+            count = count+1
+            if (count ==10):
+                count = 0
+            gyro = numpy.sum(gyro_buffer)/10.0
+            accel = numpy.sum(accel_buffer)/10.0
+            #print "accel: " +str(accel)
+            #print "gyro: " + str(gyro)
+            if (accel > ACCEL_THRESH):
+                if (gyro > GYRO_THRESH):
+                    if (key == oldKey):
+                        wristCount = wristCount+1
+                        motion_table.update(dict(date=key,elbow=elbowCount,wrist=wristCount),['date'])
+                    else:
+                        elbowCount = 0
+                        wristCount = 1
+                        motion_table.insert(dict(date=key, elbow = elbowCount, wrist = wristCount))
+                    print "update database: current hour, wrist_count"
                 else:
-                    elbowCount = 0
-                    wristCount = 1
-                    motion_table.insert(dict(date=key, elbow = elbowCount, wrist = wristCount))
-                print "update database: current hour, wrist_count"
-            else:
-                if (key == oldKey):
-                    elbowCount = elbowCount+1
-                    motion_table.update(dict(date=key,elbow=elbowCount,wrist=wristCount),['date'])
-                else:
-                    elbowCount = 1
-                    wristCount = 0
-                    motion_table.insert(dict(date=key, elbow = elbowCount, wrist = wristCount))
-                print "update database: current hour, elbow_count"                
+                    if (key == oldKey):
+                        elbowCount = elbowCount+1
+                        motion_table.update(dict(date=key,elbow=elbowCount,wrist=wristCount),['date'])
+                    else:
+                        elbowCount = 1
+                        wristCount = 0
+                        motion_table.insert(dict(date=key, elbow = elbowCount, wrist = wristCount))
+                    print "update database: current hour, elbow_count"                
+            
+    #        try:
+    #            force = ser.readline().split()
+    #        except serial.SerialException:
+    #            print "disconnected"
+    #            break
+    #        if (checkInput(force, "FSR: ")):
+    #            force = [float(force[1]),float(force[2]),float(force[3])]
+    #            force_table.insert(dict(date=key, FSR0 = force[0], FSR1 = force[1]), FSR2= force[2])
         
-#        try:
-#            force = ser.readline().split()
-#        except serial.SerialException:
-#            print "disconnected"
-#            break
-#        if (checkInput(force, "FSR: ")):
-#            force = [float(force[1]),float(force[2]),float(force[3])]
-#            force_table.insert(dict(date=key, FSR0 = force[0], FSR1 = force[1]), FSR2= force[2])
+        oldKey = key
     
-    oldKey = key
-
-print "DCed"
+    print "DCed"
 #key = str(datetime.date.today()) + " " + str(datetime.datetime.now().hour)
 #db = dataset.connect('sqlite:///nbedmbed.db')        
 #table = db['motion']
---
+++ server.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 09 14:47:38 2015
+
+@author: Bill
+"""
+
+
+import web
+import dataset
+db = dataset.connect('sqlite:///nbedmbed.db')  
+
+urls = (
+        '/motion_pie', 'update_motion_pie', 
+        '/motion_bar', 'update_motion_pie')
+
+app = web.application(urls, globals())
+
+class update_motion_pie:        
+    def GET(self):
+        print {'fake1': 1, 'fake2': 2, 'fake', 3}
+
+
+if __name__ == "__main__":
+    app.run()
